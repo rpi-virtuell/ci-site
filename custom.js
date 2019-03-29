2500d83313320c0cb6fefa7ad14438c871ba72ac
Ludger Mueller <postbox@artbeit.de>
2019-03-29 12:52:00 +0100

    enhance sidebar off page behaviour add closing X

--- custom.js
+++ custom.js
@@ -3,14 +3,19 @@
 jQuery(function ($) {
 
     if ($('.archive-sidebar').length > 0) {
-$archiveSidebar = $('.archive-sidebar');
-$contentColumn = $('.results');
-$contentHeader = $('header');
-$archiveSidebar.prepend('<div class="sidebarswitch">Info</div>');
-$archiveSidebar.on('click',function(e){
-    $(this).toggleClass('open');
-    $contentColumn.toggleClass('fade');
-    $contentHeader.toggleClass('fade');
-})
+        $archiveSidebar = $('.archive-sidebar');
+        $contentColumn = $('.results');
+        $contentHeader = $('header');
+        $archiveSidebar.prepend('<div class="sidebarswitch">Info </div><div class="sidebarClose">+</div>');
+        $('.sidebarswitch').on('click', function (e) {
+            $(this).parent().toggleClass('open');
+            $contentColumn.toggleClass('fade');
+            $contentHeader.toggleClass('fade');
+        });
+        $('.sidebarClose').on('click',function (e) {
+            $(this).parent().toggleClass('open');
+            $contentColumn.toggleClass('fade');
+            $contentHeader.toggleClass('fade');
+        })
     }
-})
+});
